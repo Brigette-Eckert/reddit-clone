c65d52bf86d20b80dfbe9918d19322a93315e40d
Brigette Eckert <Brigette.Eckert@live.com>
2017-04-29 16:15:26 -0700

    added post adn homepage compotents and basic routing file

---
+++ src/app/app.routing.ts
@@ -0,0 +1,5 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PostComponent } from './post/post.component';
+
